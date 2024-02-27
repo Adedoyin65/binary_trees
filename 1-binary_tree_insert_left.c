811bd8b8fcbfec6e5548983d5c4bee564d60e32a
Ogunyemi Daniel <ogunyemidaniel65@gmail.com>
2024-02-27 01:24:27 -0800

    Binary trees- A non-linear data structure

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -18,7 +18,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	}
 	new->n = value;
 	new->parent = parent;
-	new->left = NULL;
+	new->left = parent->left;
 	new->right = NULL;
 	if (parent->left == NULL)
 	{
